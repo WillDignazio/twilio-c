c6218d00098e8cc89e5078399a15beb8cb1af0b2
William Dignazio <slackwill@csh.rit.edu>
2012-07-29 23:44:47 -0400

    Finished the simple html encoding functionality that I had been working on. Segfaults are the worst.

---
+++ util/encodetest.c
@@ -0,0 +1,32 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  encodetest.c
+ *
+ *    Description:  Encoding test app
+ *
+ *        Version:  1.0
+ *        Created:  07/29/2012 08:08:03 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  William Dignazio (slackwill), slackwill@csh.rit.edu
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <twilio.h> 
+
+/* Simple encoded string test utility. Makes sure that the encoded string
+ * functionality I wrote works. */ 
+int main(int argc, char *argv[]) { 
+  	if(argv[1]) { 
+	  	printf("Before html_encode: %s\n", argv[1]); 
+		char *c; 
+		html_encode(argv[1], &c); 
+		printf("After html_encode: %s\n", c); 
+	}
+	return 0; 
+}
+
