b7021093a9dddd7761e4c88ae28a1e3accce53c9
Will Dignazio <wdignazio@gmail.com>
2015-03-28 00:06:38 -0400

    license: Change to MIT license
    Making it easier for folks to use privately and publicly, I'm changing
    the license to one that does not require changes to be visible publicly.

--- rest.c
+++ rest.c
@@ -1,39 +1,5 @@
 /**
- * Copyright 2012, William Dignazio
- *
- * This file is part of the twilio-c library.
- *
- * twilio-c is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * twilio-c is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with twilio.  If not, see <http://www.gnu.org/licenses/>.kh
- *
- */
-
-/*
- * =====================================================================================
- *
- *       Filename:  rest.c
- *
- *    Description:  Rest api functions for the twilio-c libraries. 
- *
- *        Version:  1.0
- *        Created:  07/28/2012 04:26:45 PM
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  William Dignazio (slackwill), slackwill@csh.rit.edu
- *   Organization:  
- *
- * =====================================================================================
+ * Copyright 2015, William Dignazio
  */
 #include <stdlib.h>
 #include <stdio.h> 
